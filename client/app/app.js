69f81fa729e317261373b11f765b6f590c0aa5e9
Dimitri <couwenberg@gmail.com>
2018-01-09 20:59:58 +1100

    before using require

--- client/app/app.js
+++ client/app/app.js
@@ -15,6 +15,7 @@ angular.module('app', [
     // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
     // #how-to-configure-your-server-to-work-with-html5mode
     $locationProvider.html5Mode(true).hashPrefix('!');
+    console.log('Components=',Components);
   })
 
   .component('app', AppComponent);
