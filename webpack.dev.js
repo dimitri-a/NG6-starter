d6e117d25a9c04e8670b19095864751346a1f34d
Dimitri <couwenberg@gmail.com>
2018-02-07 08:33:12 +1100

    trying to run webpack-server

---
+++ webpack.dev.js
@@ -0,0 +1,51 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+  cache: true,
+  devtool: "eval", //or cheap-module-eval-source-map
+  entry: {
+    app: path.join(__dirname, "client/app", "app.js")
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "newapp.js",
+    chunkFilename: "[name].js"
+  },
+  plugins: [
+    //Typically you'd have plenty of other plugins here as well
+    new webpack.DllReferencePlugin({
+      context: path.join(__dirname, "client"),
+      manifest: require("./dll/vendor-manifest.json")
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel",
+        include: [
+          path.join(__dirname, "client") //important for performance!
+        ],
+        query: {
+          cacheDirectory: true, //important for performance
+          plugins: ["transform-regenerator"],
+          presets: ["es2015", "stage-0"]
+        }
+      },
+      { test: /\.(scss|sass)$/, loader: 'style!css!sass' },
+      { test: /\.html$/, loader: 'raw' },
+      { test: /\.css$/, loader: 'style!css' }
+    ]
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"],
+    root: path.resolve(__dirname, "client"),
+    modulesDirectories: ["node_modules"]
+  }
+};
