08361ef407aefcbbbb265090fbc7e2dd948f5495
Dimitri <couwenberg@gmail.com>
2018-02-07 18:57:04 +1100

    vendor bundle with angular

--- webpack.dll.js
+++ webpack.dll.js
@@ -1,46 +1,11 @@
 var path = require("path");
 var webpack = require("webpack");
 
+
+
 module.exports = {
   entry: {
-    vendor: [
-      "angular",
-      "angular-ui-router",
-      "fs",
-      "normalize.css",
-      "angular-mocks",
-      "babel-core",
-      "babel-loader",
-      "babel-plugin-transform-runtime",
-      "babel-polyfill",
-      "babel-preset-es2015",
-      "babel-preset-stage-0",
-      "babel-register",
-      "babel-runtime",
-      "browser-sync",
-      "chai",
-      "connect-history-api-fallback",
-      "css-loader",
-      "del",
-      "fs-walk",
-      "gulp",
-      "gulp-rename",
-      "gulp-template",
-      "gulp-util",
-      "html-webpack-plugin",
-      "lodash",
-      "ng-annotate-loader",
-      "node-libs-browser",
-      "node-sass",
-      "raw-loader",
-      "run-sequence",
-      "sass-loader",
-      "style-loader",
-      "supports-color",
-      "webpack",
-      "webpack-dev-middleware",
-      "webpack-hot-middleware",
-      "yargs"]
+    dependencies: ["angular"],
   },
 
   output: {
