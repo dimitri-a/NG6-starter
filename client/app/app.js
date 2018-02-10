c4f26fef152c70957b1c27db2937a90a4d840162
Dimitri <couwenberg@gmail.com>
2018-02-10 12:58:51 +1100

    ui-view put in and working

--- client/app/app.js
+++ client/app/app.js
@@ -1,21 +1,24 @@
 import angular from 'angular';
-// import uiRouter from 'angular-ui-router';
-// import Common from './common/common';
-// import Com from './components/components'
-// import AppComponent from './app.component';
+import uiRouter from 'angular-ui-router';
+ import Common from './common/common';
+ import Com from './components/components'
+import AppComponent from './app.component';
 // import 'normalize.css';
 
 angular.module('app', [
-   ])
-  // .config(($locationProvider) => {
-  //   "ngInject";
-  //   // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
-  //   // #how-to-configure-your-server-to-work-with-html5mode
-  //   $locationProvider.html5Mode(true).hashPrefix('!');
-  //
-  // })
+    uiRouter,
+  Common,
+  Com
+  ])
+  .config(($locationProvider) => {
+    "ngInject";
+    // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
+    // #how-to-configure-your-server-to-work-with-html5mode
+    $locationProvider.html5Mode(true).hashPrefix('!');
+
+  })
   .controller('ctrl',function($scope){
     $scope.text='hello again, and again';
+    console.log('hey new');
   })
-
- /// .component('app', AppComponent);
+  .component('app', AppComponent);
